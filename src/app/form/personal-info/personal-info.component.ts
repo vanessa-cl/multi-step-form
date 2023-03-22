6b9d3765e6449722fd1be16b55522f85518d4d03
Vanessa Lima <vanessalima951@gmail.com>
2023-03-22 13:14:40 -0300

    personal info form basic styling and extends and common styles created

--- src/app/form/personal-info/personal-info.component.ts
+++ src/app/form/personal-info/personal-info.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-personal-info',
   templateUrl: './personal-info.component.html',
-  styleUrls: ['./personal-info.component.sass']
+  styleUrls: ['./personal-info.component.sass', '../../sass/utilities/_common.sass']
 })
 export class PersonalInfoComponent {
 
