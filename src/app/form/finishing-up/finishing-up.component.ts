b1e65b1af14451af53ae1097aa423748c700dfc3
Vanessa Lima <vanessalima951@gmail.com>
2023-03-29 09:44:56 -0300

    finishing up page styling

--- src/app/form/finishing-up/finishing-up.component.ts
+++ src/app/form/finishing-up/finishing-up.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-finishing-up',
   templateUrl: './finishing-up.component.html',
-  styleUrls: ['./finishing-up.component.sass']
+  styleUrls: ['./finishing-up.component.sass', '../../sass/utilities/_common.sass']
 })
 export class FinishingUpComponent {
 
