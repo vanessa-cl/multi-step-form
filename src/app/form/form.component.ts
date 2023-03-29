dc9083ab369ee4169fe99c88d76deeacb4905f66
Vanessa Lima <vanessalima951@gmail.com>
2023-03-29 11:23:32 -0300

    thank you and not found pages styling

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.sass']
+  styleUrls: ['./form.component.sass', '../sass/utilities/_common.sass']
 })
 export class FormComponent {
 
--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.sass']
+  styleUrls: ['./not-found.component.sass', '../sass/utilities/_common.sass']
 })
 export class NotFoundComponent {
 
